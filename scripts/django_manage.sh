b6a168ee14f09a78fb06d8e84e22caa3cc263f25
ZombieToof <z0mb13t00f@gmail.com>
2014-05-07 21:38:24 +0200

    rename django_shell.sh to django_manage.sh and pass arguments
    Instead of a dedicated script to call the shell, this will call
    manage.py and pass all following arguments to it.
    So you can call `./scripts/django_manages.sh shell` or
    `./scripts/django_manage.sh makemigrations abcapp` followed
    by `./scripts/django_manage.sh migrate`.

---
+++ scripts/django_manage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vagrant ssh -c "/home/vagrant/gcdjango/bin/python /vagrant/projects/gcabc/manage.py $*"
--- scripts/django_shell.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-vagrant ssh -c '/home/vagrant/gcdjango/bin/python /vagrant/projects/gcabc/manage.py shell'
